032103196c9789b7e4ad2dcb2fc69ae1fc78d48d
shamanoff <shamanoff@gmail.com>
2017-02-15 14:55:30 +0200

    html

--- src/main/java/com/exercisegenerator/model/ExerciseConditions.java
+++ src/main/java/com/exercisegenerator/model/ExerciseConditions.java
@@ -1,25 +1,16 @@
 package com.exercisegenerator.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
-import javax.persistence.*;
-
-@Entity
+@Component
 @Data
-@Table(name = "conditions")
-@AllArgsConstructor
 public class ExerciseConditions {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+
     private final Long id;
-    @Column
     private final int fromInt;
-    @Column
     private final int toInt;
-    @Column
     private final String mathCondition;
-    @Column
     private final int exercisesCount;
 
 }
